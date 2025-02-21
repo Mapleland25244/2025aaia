a3d5d24c72ad5d8c89adea0bff8d6c168673c3f6
Mapleland25244 <asdfasdf102192@gmail.com>
2025-02-21 11:55:38 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,13 @@
+/// week01-1.cpp
+/// 在瘋狂程設(練習)寫好的code，全選，ctrl+c
+/// Codeblocks File-New-Empty 空白檔案，ctrl+s存檔
+#include <stdio.h>///使用standard 標準 input/output
+
+int main(){
+
+	int a, b;/// 整數 a, b
+	scanf("%d%d", &a, &b);///調入資料(小心 "%d%d" 後面「逗號」)
+	/// 兩個%d%d  &   &
+	printf("%d", a + b); /// 印出資料, 把答案印出來
+
+}
