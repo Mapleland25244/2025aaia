56b275537faa9fcb55ce4233efacba21722d3494
Mapleland25244 <asdfasdf102192@gmail.com>
2025-03-14 12:00:43 +0800

    week04

---
+++ week04/week04-3.py
@@ -0,0 +1,12 @@
+# week04-3.py
+#試各種 for迴圈
+N = 5
+for i in range(0,N):
+  print(i,end="")
+print("\n") # 跳行
+for i in range(1,N+1):
+  print(i,end="")
+print("\n") # 跳行
+for i in range(N-1,-1,-1):
+  print(i,end="")
+print("\n") # 跳行
